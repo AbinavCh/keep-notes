4a28e484f20ffe52022d2b0eeaedc5207c231469
Abinav Ch <abinavch06@gmail.com>
2021-05-09 18:47:56 +0530

    Complete Project

--- src/App.js
+++ src/App.js
@@ -1,190 +1,145 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import Note from './components/Note.js'
+import Note from './components/Note.js';
 import CreateArea from './components/CreateArea.js';
-import firebase, { fire }  from './components/firebase';
-
-/* */
-// import { React, useState, useEffect } from 'react'
-import Login from './Login-components/Login'
-// import fire from './components/firebase'
-import Hello from './Login-components/Hello'
-/* */
+import firebase, { fire } from './components/firebase';
+import Login from './Login-components/Login';
 
 function useArray2() {
-
-  const [ array2, setArray2 ] = useState([]);
+  const [array2, setArray2] = useState([]);
 
   useEffect(() => {
-    
-    firebase.firestore().collection('Note')
-    .onSnapshot((snapshot) => {
-      const newArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      setArray2(newArray);
-    })
-  }, [])
+    firebase
+      .firestore()
+      .collection('Note')
+      .onSnapshot((snapshot) => {
+        const newArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setArray2(newArray);
+      });
+  }, []);
 
   return array2;
 }
 
 function App() {
-
   const array2 = useArray2();
 
   function deletefireNote(id) {
-    
     firebase.firestore().collection('Note').doc(id).delete();
   }
 
-  // const [ array, setArray ] = useState([]);
-
-  // function addNote(newNote) {
-  //   setArray((previous) => {
-  //     return [...previous, newNote];
-  //   });
-  // }
-
-  // function deleteNote(id) {
-  //   setArray((previous) => {
-  //     return previous.filter((item, index) => {
-  //       return index !== id;
-  //     })
-  //   })
-  // }
-
-
-  /* */
-  const [user, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const [hasaccount, setHasaccount] = useState(false);
 
   const clearInputs = () => {
-    setEmail("");
-    setPassword("");
-  }
+    setEmail('');
+    setPassword('');
+  };
 
   const clearErrors = () => {
     setPasswordError('');
     setEmailError('');
-  }
+  };
 
   const handleLogin = () => {
     clearErrors();
     fire
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch((error) => {
-      switch(error.code) {
-        case "auth/invalid-email":
-        case "auth/user-disabled":
-        case "auth/user-not-found":
-          setEmailError(error.message);
-          break;
-        case "auth/wrong-password":
-          setPasswordError(error.message);
-          break;  
-      }
-    });
-  }
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/invalid-email':
+          case 'auth/user-disabled':
+          case 'auth/user-not-found':
+            setEmailError(error.message);
+            break;
+          case 'auth/wrong-password':
+            setPasswordError(error.message);
+            break;
+        }
+      });
+  };
 
   const handleSignup = () => {
     clearErrors();
     fire
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .catch((error) => {
-      switch (error.code) {
-        case "auth/email-already-in-use":
-        case "auth/invalid-email":
-          setEmailError(error.message);
-          break;
-        case "auth/weak-password":
-          setPasswordError(error.message);
-          break;  
-    }
-    });
-  }
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/email-already-in-use':
+          case 'auth/invalid-email':
+            setEmailError(error.message);
+            break;
+          case 'auth/weak-password':
+            setPasswordError(error.message);
+            break;
+        }
+      });
+  };
 
   const handleLogout = () => {
-    fire
-    .auth().signOut()
-  }
+    fire.auth().signOut();
+  };
 
   const authListener = () => {
     fire.auth().onAuthStateChanged((user) => {
-      if(user) {
+      if (user) {
         clearInputs();
         setUser(user);
-      }
-      else {
-        setUser("");
+      } else {
+        setUser('');
       }
     });
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     authListener();
-  }, [])
-
+  }, []);
 
   /* */
 
-
   return (
-    <div className="App">
-
-    {user ? (
-      <>
-      <Header handleLogout={handleLogout} />
-      {/* <Hello handleLogout={handleLogout} /> */}
-      <CreateArea />
-        {/* onAdd = {addNote} /> */}
-      {/* {array.map((item, index) => {
-        return (
-          <Note 
-          key={index}
-          id={index}
-          clicked = {deleteNote}
-          title={item.title}
-          content={item.content}
-          />
-        );
-      })} */}
-      {array2.map((ele) => {
-        return (
-          <Note 
-            key = {ele.id}
-            id = {ele.id}
-            clicked = {deletefireNote}
-            title={ele.noteTitle}
-            content={ele.noteContent}
-          />
-        )})
-      }
-      <Footer />
-        
+    <div className='App'>
+      {user ? (
+        <>
+          <Header handleLogout={handleLogout} />
+          <CreateArea />
+          {array2.map((ele) => {
+            return (
+              <Note
+                key={ele.id}
+                id={ele.id}
+                clicked={deletefireNote}
+                title={ele.noteTitle}
+                content={ele.noteContent}
+              />
+            );
+          })}
+          <Footer />
         </>
-        ):(
-      <Login 
-        email={email}
-        password={password}
-        setEmail={setEmail}
-        setPassword={setPassword}
-        handleLogin={handleLogin}
-        handleSignup={handleSignup}
-        hasaccount={hasaccount}
-        emailError={emailError}
-        passwordError={passwordError}
-        setHasaccount={setHasaccount}
-      />
-    )}   
+      ) : (
+        <Login
+          email={email}
+          password={password}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+          handleSignup={handleSignup}
+          hasaccount={hasaccount}
+          emailError={emailError}
+          passwordError={passwordError}
+          setHasaccount={setHasaccount}
+        />
+      )}
     </div>
   );
 }
--- src/Login-components/firebase.js
+++ src/Login-components/firebase.js
@@ -1,27 +1,14 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyART-X4BTiQgZUQpVV-Rf4Mf6qxwxG6nhg",
-    authDomain: "keep-notes-32def.firebaseapp.com",
-    projectId: "keep-notes-32def",
-    storageBucket: "keep-notes-32def.appspot.com",
-    messagingSenderId: "986878073716",
-    appId: "1:986878073716:web:b4a7540fc8c4cdf071aa21"
-  };
-  // Initialize Firebase
-  const fire = firebase.initializeApp(firebaseConfig);
+  apiKey: 'AIzaSyART-X4BTiQgZUQpVV-Rf4Mf6qxwxG6nhg',
+  authDomain: 'keep-notes-32def.firebaseapp.com',
+  projectId: 'keep-notes-32def',
+  storageBucket: 'keep-notes-32def.appspot.com',
+  messagingSenderId: '986878073716',
+  appId: '1:986878073716:web:b4a7540fc8c4cdf071aa21',
+};
+// Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
 
-  export default fire;
-
-
-  
-// var firebaseConfig = {
-//     apiKey: "AIzaSyART-X4BTiQgZUQpVV-Rf4Mf6qxwxG6nhg",
-//     authDomain: "keep-notes-32def.firebaseapp.com",
-//     projectId: "keep-notes-32def",
-//     storageBucket: "keep-notes-32def.appspot.com",
-//     messagingSenderId: "986878073716",
-//     appId: "1:986878073716:web:b4a7540fc8c4cdf071aa21"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
+export default fire;
